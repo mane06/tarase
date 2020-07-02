82f00f48b2e49d427fcba088befa3c4924df232c
mane06 <tecnotop36@gmail.com>
2020-07-01 19:20:36 -0500

    version 1

---
+++ assets/js/table-data.js
@@ -0,0 +1,30 @@
+
+$('.lista').DataTable({
+		"searching": false,
+		"paging":   false,
+	    "bInfo": false,
+	    "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "ordering": false,
+        "sScrollY": "100px"
+	});
+
+$('.base').DataTable({
+		"searching": false,
+		"paging":   false,
+	    "bInfo": false,
+	    "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "ordering": false,
+        "sScrollY": "110px"
+	});
+
+$('.principal').DataTable({
+		"searching": false,
+		"paging":   false,
+	    "bInfo": false,
+	    "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "ordering": false,
+        "sScrollY": "175px"
+	});
---
+++ assets/js/tables.js
@@ -0,0 +1,14 @@
+$(function(e) {
+	$('#lista').DataTable({
+		"searching": false,
+		"paging":   false,
+	    "bInfo": false,
+	    "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "ordering": false,
+        "sScrollY": "170px"
+	});
+
+
+});
+
